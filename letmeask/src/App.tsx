3db49a3037532fdde1b257e771ed64b595d96e66
antonioRenato <antoniorpfs@gmail.com>
2023-03-09 21:07:42 -0300

    created button

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,6 +1,12 @@
+import { Button } from "./components/Button";
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Button>Clique Aqui</Button>
+      <Button />
+      <Button />
+    </div>
   );
 }
 
