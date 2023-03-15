bd66a1ba58ad57add9dda5e6e965ac10feb3ab2a
antonioRenato <antoniorpfs@gmail.com>
2023-03-15 20:26:09 -0300

    start create css

--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -2,16 +2,18 @@ import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
+import '../styles/auth.scss'
+
 export function Home(){
   return (
-    <div>
+    <div id="page-auth">
       <aside>
         <img src={illustrationImg} alt="ilustração simbolizando perguntas e respostas" />
         <strong>Crie salas de Q&amp;A ai-vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo real</p>
       </aside>
       <main>
-        <div>
+        <div className="main-content">
           <img src={logoImg} alt="letmeask" />
           <button>
             <img src={googleIconImg} alt="Logo do google" />
